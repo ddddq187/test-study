763d3f449bacfc5014b470e38287f9362c7ec0e5
Wang_Dongqiao <ddddq187@gmail.com>
2024-04-07 15:45:33 +0800

    first commit test

---
+++ algo_intro_code/test_cpp_code/test_g++_enable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 100,b = 10;
+    cout << a + b <<endl;
+    cout << "hello" << endl;
+    cout << "hello" << endl;
+    cout << "hello" << endl;
+    return 0;
+}
